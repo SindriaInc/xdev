691aa304565633ac2ec1535d37f0ec528ed1476f
lucapitzoi <luca.pitzoi@sindria.org>
2019-05-06 20:30:48 +0000

    Added config, Edit startup.sh

--- src/startup.sh
+++ src/startup.sh
@@ -1,9 +1,15 @@
 #!/usr/bin/env bash
 
-#CONTAINER_GATEWAY=`/sbin/ip route|awk '/default/ { print $3 }'`
+CONTAINER_GATEWAY=`/sbin/ip route|awk '/default/ { print $3 }'`
 
 # Adding host machine hostname
-#echo -e "\n# Hostname for gateway" >> /etc/hosts
-#echo -e "${CONTAINER_GATEWAY}\tdocker.host.internal\n" >> /etc/hosts
+echo -e "\n# Hostname for gateway" >> /etc/hosts
+echo -e "${CONTAINER_GATEWAY}\tdocker.host.internal\n" >> /etc/hosts
 
-/usr/bin/supervisord -n -c /etc/supervisor/supervisord.conf
+# Add custom hosts
+if [ ! -f "/home/sindria/config/etc/.done" ]; then
+  cat /home/sindria/config/etc/hosts >> /etc/hosts
+  touch /home/sindria/config/etc/.done
+fi  
+
+su sindria -c "export DISPLAY=$tDISPLAY && /usr/bin/i3"
