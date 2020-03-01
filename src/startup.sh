30c4dffde3ef088dbb5dd4fabee1d312c9943c2b
lucapitzoi <luca.pitzoi@sindria.org>
2020-03-01 21:18:52 +0100

    Added custom i3 config

--- src/startup.sh
+++ src/startup.sh
@@ -4,6 +4,11 @@
 if [ ! -f "/home/sindria/config/etc/.done" ]; then
   cat /home/sindria/config/etc/hosts >> /etc/hosts
   touch /home/sindria/config/etc/.done
-fi  
+fi
+
+# Custom I3 configuration
+if [ -f "/home/sindria/config/i3/config" ]; then
+  cp /home/sindria/config/i3/config /home/sindria/.config/i3/config
+fi
 
 su sindria -c "export DISPLAY=$DISPLAY && /usr/bin/i3"
