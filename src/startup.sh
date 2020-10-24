915749a2bcea769a32c37d31ed81b2bb0c936f8a
lucapitzoi <luca.pitzoi@sindria.org>
2020-10-24 19:30:42 +0200

    Added override env startup, Fix example deployment

--- src/startup.sh
+++ src/startup.sh
@@ -1,14 +1,34 @@
 #!/usr/bin/env bash
 
-# Add custom hosts
-#if [ ! -f "/home/sindria/config/etc/.done" ]; then
-#  cat /home/sindria/config/etc/hosts >> /etc/hosts
-#  touch /home/sindria/config/etc/.done
-#fi
+
+# Override default sindria user password by env
+if [ "$DEFAULT_SINDRIA_USER_PASSWORD" != "sindria" ]; then
+    echo "sindria:${DEFAULT_SINDRIA_USER_PASSWORD}" | chpasswd
+fi
+
+
+# Override host user uid by env
+if [ "$HOST_USER_UID" != "1000" ]; then
+    usermod -u $HOST_USER_UID sindria && groupmod sindria -g $HOST_USER_UID
+fi
+
+
+# Override docker host uid by env
+if [ "$HOST_DOCKER_GROUP_UID" != "975" ]; then
+    groupmod docker -g $HOST_DOCKER_GROUP_UID
+fi
+
+
+# Override timezone by env
+if [ "$TZ" != "" ] || [ "$TZ" != "Europe/Rome" ]; then
+    ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone
+fi
+
 
 # Custom I3 configuration
 if [ -f "/home/sindria/config/i3/config" ]; then
   cp /home/sindria/config/i3/config /home/sindria/.config/i3/config
 fi
+
 
 su sindria -c "export DISPLAY=$DISPLAY && /usr/bin/i3"
