dc7915b906d550fe63b672b878b7c955b64226d9
Luca Pitzoi <luca.pitzoi@sindria.org>
2020-11-02 03:51:43 +0100

    Add new file

---
+++ src/resources/scripts/i3-gaps.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+sudo dnf install -y libxcb-devel xcb-util-keysyms-devel xcb-util-devel xcb-util-wm-devel xcb-util-xrm-devel yajl-devel libXrandr-devel startup-notification-devel libev-devel xcb-util-cursor-devel libXinerama-devel libxkbcommon-devel libxkbcommon-x11-devel pcre-devel pango-devel git gcc automake i3status i3lock make
+cd /tmp
+git clone https://www.github.com/Airblader/i3 i3-gaps && cd i3-gaps
+autoreconf --force --install
+rm -rf build/
+mkdir -p build && cd build/
+../configure --prefix=/usr --sysconfdir=/etc --disable-sanitizers
+make
+sudo make install
