0ddf94e12d2a7f6a0233bc5322984d94ad740677
lucapitzoi <luca.pitzoi@sindria.org>
2021-07-01 19:08:34 +0200

    Fix

--- build.sh
+++ build.sh
@@ -13,13 +13,10 @@ fi
 IMAGE_NAME=$1
 TAG_VERSION=$2
 
-HOST_USER_UID=1000
 XDEV_DISPLAY=:0.0
 
 docker build ./src \
     --tag ${IMAGE_NAME}:${TAG_VERSION} \
     --tag ${IMAGE_NAME}:latest \
     --build-arg TAG_VERSION=${TAG_VERSION} \
-    --build-arg HOST_USER_UID=${HOST_USER_UID} \
-    --build-arg HOST_DOCKER_GROUP_UID=${HOST_DOCKER_GROUP_UID} \
     --build-arg XDEV_DISPLAY=${XDEV_DISPLAY}
