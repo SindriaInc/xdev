15bbd55f4079444e7395da2e2c2bdc09bd1f2ee5
lucapitzoi <luca.pitzoi@sindria.org>
2022-01-29 18:38:25 +0100

    Added deployments

--- src/resources/bin/entrypoint.sh
+++ src/resources/bin/entrypoint.sh
@@ -2,10 +2,3 @@
 
 chown root:docker /var/run/docker.sock
 chmod 660 /var/run/docker.sock
-
-#
-if [ ! -f ${SINDRIA_USER_HOME}/ ]; then
-    chmod 700 ${SINDRIA_USER_HOME}/.aws
-    chmod 600 ${SINDRIA_USER_HOME}/.aws/*
-fi
-
