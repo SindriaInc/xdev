efaefac43e3ff871b1a26d03a1d45cbb91f3aadd
lucapitzoi <luca.pitzoi@sindria.org>
2022-01-29 16:58:09 +0100

    FIx xdev install utility

--- bin/xdev-install.sh
+++ bin/xdev-install.sh
@@ -14,7 +14,19 @@ if [[ -z "$1" ]]; then
     exit 1
 fi
 
+if [[ -z "$2" ]]; then
+    echo -e "${YELLOW}Provide git username as second argument${NC}"
+    exit 1
+fi
+
+if [[ -z "$3" ]]; then
+    echo -e "${YELLOW}Provide git user email as third argument${NC}"
+    exit 1
+fi
+
 XDEV_NAME=$1
+GIT_USERNAME=$2
+GIT_EMAIL=$3
 
 
 if [ ! -d ${HOME}/${XDEV_NAME} ]; then
@@ -48,10 +60,17 @@ if [ ! -f ${HOME}/${XDEV_NAME}/.cloud ]; then
     echo -e "${BLUE}Not found cloud setup, downloading...${NC}"
     cd ${HOME}/${XDEV_NAME}
     wget --no-check-certificate https://raw.githubusercontent.com/SindriaInc/xdev/master/deployments/compose-wsl.yml
-    wget --no-check-certificate https://raw.githubusercontent.com/SindriaInc/xdev/master/deployments/.env.wsl
+    wget --no-check-certificate https://raw.githubusercontent.com/SindriaInc/xdev/master/deployments/.env.example
     mv compose-wsl.yml docker-compose.yml
-    mv .env.wsl .env
+    mv .env.example .env
+    cd ${HOME}
 fi
 echo
+
+echo -e "${BLUE}Setup cached env file...${NC}"
+sed -i -E "s|@@USER@@|${USER}|g" ${HOME}/${XDEV_NAME}/.env
+sed -i -E "s|@@XDEV_NAME@@|${XDEV_NAME}|g" ${HOME}/${XDEV_NAME}/.env
+sed -i -E "s|@@GIT_USERNAME@@|${GIT_USERNAME}|g" ${HOME}/${XDEV_NAME}/.env
+sed -i -E "s|@@GIT_EMAIL@@|${GIT_EMAIL}|g" ${HOME}/${XDEV_NAME}/.env
 
 echo -e "${BLUE}Done${NC}"
